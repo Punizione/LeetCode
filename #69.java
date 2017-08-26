cadbe9b013ef12c36dfc72c7012e039a3ee76d78
Punizione <1273435147@qq.com>
2017-08-26 12:34:29 +0800

    Add files via upload

---
+++ #69.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Test{
+	public static void main(String[] args){
+		System.out.println(mySqrt(5));
+	}
+    public static int mySqrt(int x) {
+        return (int)Math.sqrt(x);
+    }
+		
+}
