e8b64cbe98c8f2f9ffbb7033200fb94edbd3aefd
Punizione <1273435147@qq.com>
2017-08-28 11:19:12 +0800

    Add files via upload

---
+++ #50.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Test{
+	public static void main(String[] args){
+		System.out.println(myPow(5.0,2));
+	}
+    public static double myPow(double x, int n) {
+        return Math.pow(x,(double)n);
+    }
+		
+}
