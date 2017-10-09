de60354c0200d3122e04bb9d2aada61bc95be6c1
Punizione <1273435147@qq.com>
2017-10-09 17:51:17 +0800

    Add files via upload

---
+++ #100.java
@@ -0,0 +1,13 @@
+import java.util.*;
+public class Test{
+	public static void main(String[] args){
+		isPalindrome();
+	}
+	public boolean isSameTree(TreeNode p, TreeNode q) {
+		if(p==null&q==null){
+			return true;
+		}
+		return p!=null&&q!=null&&p.val==q.val&&isSameTree(p.left,q.left)&&isSameTree(p.right,q.right);
+
+	}
+}
