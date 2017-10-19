03e8f276474becc3217c2ac1e778a5d355d7c965
Punizione <1273435147@qq.com>
2017-10-19 18:55:09 +0800

    Add files via upload

---
+++ #344.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class Test{
+	public static void main(String[] args){
+		isSubsequence("abc","ahbgdc");
+	}
+	public String reverseString(String s) {
+		StringBuilder sb = new StringBuilder(s);
+		return sb.reverse().toString();
+	}
+}
