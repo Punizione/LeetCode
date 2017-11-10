f400e80931cb7b913812906d5e848e8b42a9f342
Punizione <1273435147@qq.com>
2017-11-10 18:39:28 +0800

    Add files via upload

---
+++ #541.java
@@ -0,0 +1,17 @@
+import java.util.*;
+public class Test{
+	public static void main(String[] args){
+		isSubsequence("abc","ahbgdc");
+	}
+	public String reverseStr(String s, int k) {
+		char[] ca = s.toCharArray();
+		for (int left = 0; left < ca.length; left += 2 * k) {
+			for (int i = left, j = Math.min(left + k - 1, ca.length - 1); i < j; i++, j--) {
+				char tmp = ca[i];
+				ca[i] = ca[j];
+				ca[j] = tmp;
+			}
+		}
+		return new String(ca);
+	}
+}
