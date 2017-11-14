6ca441b14d2b42842817038b55fb8120f22e244e
Punizione <1273435147@qq.com>
2017-11-14 20:52:00 +0800

    Add files via upload

--- #1Better.java
+++ #1Better.java
@@ -8,16 +8,16 @@ public class Test {
 		twoSum(nums,target);
 	}
 	public static int[] twoSum(int[] nums, int target) {
-		int[] result = new int[2];
-		Map<Integer, Integer> map = new HashMap<Integer, Integer>();
-		for (int i = 0; i < numbers.length; i++) {
-			if (map.containsKey(target - numbers[i])) {
-				result[1] = i + 1;
-				result[0] = map.get(target - numbers[i]);
-				return result;
-			}
-			map.put(numbers[i], i + 1);
-		}
-		return result;
+	    int[] result = new int[2];
+	    Map<Integer, Integer> map = new HashMap<Integer, Integer>();
+	    for (int i = 0; i < nums.length; i++) {
+	        if (map.containsKey(target - nums[i])) {
+	            result[1] = i ;
+	            result[0] = map.get(target - nums[i]);
+	            return result;
+	        }
+	        map.put(nums[i], i );
+	    }
+	    return result;
 	}
 }
