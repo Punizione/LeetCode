e2cf6508eacd510cd43521e430898eb970159df4
Punizione <1273435147@qq.com>
2017-11-19 10:38:45 +0800

    Add files via upload

---
+++ #258.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Test{
+	public static void main(String[] args){
+		isSubsequence("abc","ahbgdc");
+	}
+	public int addDigits(int num) {
+		return num==0?0:(num%9==0?9:(num%9));
+	}
+}
