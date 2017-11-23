b7d32bd6bb70ac9345898ae3770e5c1961326043
Punizione <1273435147@qq.com>
2017-11-23 18:02:15 +0800

    Add files via upload

---
+++ #107BFS.java
@@ -0,0 +1,35 @@
+import java.util.*;
+public class Test{
+	public static void main(String[] args){
+	//	System.out.println(isPalindrome("aacecaa"));
+		System.out.println(longestConsecutive(new int[]{1,3,4,6,8,200,1,55,2,6,8,4,9,7,54,8,15,48465,12,}));
+	}
+
+	public List<List<Integer>> levelOrder(TreeNode root) {
+		
+		LinkedList<List<Integer>> list = new LinkedList<List<Integer>>();
+		if(root == null){
+			return list;
+		}
+
+		Queue<TreeNode> queue = new LinkedList<TreeNode>();
+		queue.offer(root);
+		while(!queue.isEmpty()){
+			int level = queue.size();
+			List<Integer> subList = new ArrayList<Integer>();
+			for(int i=0;i<level;i++){
+				TreeNode node = queue.poll();
+				subList.add(node.val);
+				if(node.left !=null){
+					queue.offer(node.left);
+				}
+				if(node.right!=null){
+					queue.offer(node.right);
+				}
+			}
+			list.addFirst(subList);
+		}
+		return list;
+	}
+
+}
