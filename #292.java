ca5f903b42822caf916d893a1da0c9d27e7fdb37
Punizione <1273435147@qq.com>
2017-12-08 19:57:39 +0800

    Add files via upload

---
+++ #292.java
@@ -0,0 +1,9 @@
+import java.util.*;
+public class Test{
+	public static void main(String[] args){
+		isSubsequence("abc","ahbgdc");
+	}
+	public boolean canWinNim(int n) {
+		return (n%4!=0);
+	}
+}
