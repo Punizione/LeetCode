03988923a7584d0568cebd86d909d7d44742bbaf
Punizione <punizionefzz@gmail.com>
2017-12-31 12:34:27 +0800

    Add files via upload

---
+++ #383.java
@@ -0,0 +1,18 @@
+import java.util.*;
+public class Test{
+	public static void main(String[] args){
+		System.out.println(canConstruct("aa","aab"));
+	}
+	public static  boolean canConstruct(String ransomNote, String magazine) {
+		int[] arr = new int[26];
+		for (int i = 0; i < magazine.length(); i++) {
+			arr[magazine.charAt(i) - 'a']++;
+		}
+		for (int i = 0; i < ransomNote.length(); i++) {
+			if(--arr[ransomNote.charAt(i)-'a'] < 0) {
+				return false;
+			}
+		}
+		return true;
+	}
+}
