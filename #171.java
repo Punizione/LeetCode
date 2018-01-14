017093c00ba82829ef008493647d06161676b39f
Punizione <punizionefzz@gmail.com>
2018-01-14 15:11:42 +0800

    Add files via upload

---
+++ #171.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Test{
+	public static void main(String[] args){
+		System.out.println(canConstruct("aa","aab"));
+	}
+	public String convertToTitle(int n) {
+		int result  = 0;
+		for (int i = 0; i < s.length(); i++){
+			result *= 26;
+			result += ((s.charAt(i) - 'A') + 1);    
+		}
+		return result;
+	}
+}
