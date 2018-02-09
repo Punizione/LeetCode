f0271dee4723ccbb3784e8100d2cd09e073ee356
Punizione <punizionefzz@gmail.com>
2018-02-09 20:32:10 +0800

    Add files via upload

---
+++ #141.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class Test{
+	public static void main(String[] args){
+	//	System.out.println(isPalindrome("aacecaa"));
+		System.out.println(arrangeCoins(8));
+	}
+
+	public boolean hasCycle(ListNode head) {
+		if (head == null || head.next == null) {
+			return false;
+		}
+		ListNode slow = head;
+		ListNode fast = head.next;
+		while (slow != fast) {
+			if (fast == null || fast.next == null) {
+				return false;
+			}
+			slow = slow.next;
+			fast = fast.next.next;
+		}
+		return true;
+	}
+}
+
