32efbd6451038376f6d8deec174edf217df73edf
Punizione <punizionefzz@gmail.com>
2018-03-24 21:59:05 +0800

    Add files via upload

---
+++ #92.java
@@ -0,0 +1,30 @@
+import java.util.*;
+public class Test{
+	public static void main(String[] args){
+	//	System.out.println(isPalindrome("aacecaa"));
+		System.out.println(climbStairs(8));
+	}
+
+	public ListNode reverseBetween(ListNode head, int m, int n) {
+		if(head == null)
+			return null;
+		ListNode dummy = new ListNode(0); 
+		dummy.next = head;
+		ListNode pre = dummy; 
+		for(int i = 0; i<m-1; i++) pre = pre.next;
+		
+		ListNode start = pre.next; 
+		ListNode then = start.next; 
+
+		for(int i=0; i<n-m; i++){
+			start.next = then.next;
+			then.next = pre.next;
+			pre.next = then;
+			then = start.next;
+		}
+		
+		return dummy.next;
+		
+	}
+
+}
