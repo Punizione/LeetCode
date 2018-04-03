62757f7e62262a4fed634c9d9f2579b6391e641c
Punizione <punizionefzz@gmail.com>
2018-04-03 20:25:27 +0800

    Add files via upload

---
+++ #646UsingLambdaInJava8.java
@@ -0,0 +1,26 @@
+import java.util.*;
+public class Test{
+	public static void main(String[] args){
+	//	System.out.println(isPalindrome("aacecaa"));
+		System.out.println(climbStairs(8));
+	}
+
+	public int findLongestChain(int[][] pairs) {
+		Arrays.sort(pairs, (a, b) -> (a[0] - b[0]));
+		
+		int i, j, max = 0, n = pairs.length;
+		int dp[] = new int[n];
+	  
+		for (i = 0; i < n; i++) dp[i] = 1;
+		
+		for (i = 1; i < n; i++)
+			for (j = 0; j < i; j++)
+				if (pairs[i][0] > pairs[j][1] && dp[i] < dp[j] + 1)
+					dp[i] = dp[j] + 1;
+
+		for (i = 0; i < n; i++) if (max < dp[i]) max = dp[i];
+		
+		return max;        
+	}
+
+}
